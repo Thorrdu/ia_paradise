c8d8cb8ff47c1e0f5018bc2a6cf5d2f4a6648bd1
Thorrdu <thierry.hansens@gmail.com>
2025-03-28 23:29:49 +0100

    fix init py

--- agents/__init__.py
+++ agents/__init__.py
@@ -2,11 +2,9 @@
 Package agents pour la gestion des agents IA
 """
 
-from .main import create_crew, generate_performance_report
-from .system_agent import create_system_agent
-from .file_agent import create_file_agent
+from .main import generate_performance_report
 
-__all__ = ['create_crew', 'generate_performance_report', 'create_system_agent', 'create_file_agent']
+__all__ = ['generate_performance_report']
 
 """
 Module d'agents pour Paradis IA
